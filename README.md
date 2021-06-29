5463e8e26dcea4434244f05a553d80e29b2521d5
Saicharanlagishetty <80159866+Saicharanlagishetty@users.noreply.github.com>
2021-06-29 20:04:35 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jungle runner
+jungle runner
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canvas,backgroundImage;
+
+
+function preload(){
+
+backgroundImage = loadImage("images/Background.jpg")
+
+}
+
+function setup(){
+createCanvas(1200,500);
+
+}
+
+function draw(){
+  background(backgroundImage);
+
+
+
+}
